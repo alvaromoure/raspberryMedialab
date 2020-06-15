871e9b40fe914d0335aa973b83cb521c15f80623
alvaromoure <alvaromoure@gmail.com>
2020-06-15 12:37:17 +0200

    15 de Junio

--- displayRosetaViento.py
+++ displayRosetaViento.py
@@ -5,6 +5,13 @@ import sys
 from pygame.locals import *
 from pygame.draw import *
 
+import pygame
+import math
+import os
+import sys
+from pygame.locals import *
+from pygame.draw import *
+
 pygame.init()
 pygame.display.set_caption("minimal program")
 BLACK = 0,0,0
@@ -13,7 +20,7 @@ RED = 255,0,0
 monitor_width = pygame.display.Info().current_w
 monitor_height = pygame.display.Info().current_h
 monitor_size = [monitor_width,monitor_height]
-screen = pygame.display.set_mode(monitor_size,RESIZABLE)
+screen = pygame.display.set_mode(monitor_size,FULLSCREEN)
 
 arrow_surface = pygame.Surface((int(monitor_width/2),monitor_height),pygame.SRCALPHA)
 fullscreen = False
@@ -27,45 +34,35 @@ centro_flecha = [int((arrow_end[0]+arrow_start[0])/2),int((arrow_end[1]+arrow_st
 
 aviso_centro = [int(monitor_width-monitor_width/6),int(monitor_height/2)]
 aviso_font_size = 120
-class displaySignal(object):
-    def __init__(self,velocidad,dirViento):
-        self.velocidad = velocidad
-        self.dirViento = dirViento
-    
-    def draw(self,screen):
-        screen.fill(BLACK)
-        if (self.velocidad!=0):
-            cx = int(screen.get_width()/2)
-            cy = int(screen.get_height()/2)
-            font_size = 480
-            circle_size = 380
-            circle_width = 10
-            circulo = circle(screen,RED,(cx-circle_offset,cy),circle_size,circle_width)
-            myfont = pygame.font.SysFont("Transport",font_size)
-            velocidad = myfont.render(str(self.velocidad),1,WHITE)
-            velocidad_rotada = pygame.transform.rotate(velocidad,90)
-            velocidad_rect = velocidad_rotada.get_rect(center = circulo.center)
-            screen.blit(velocidad_rotada,velocidad_rect)
-            dibujar_flecha(self.dirViento,screen,WHITE,WHITE,arrow_start,arrow_end,50)
-            
-            fuente_aviso = pygame.font.SysFont("Transport",aviso_font_size)
-            aviso = fuente_aviso.render("VIENTO FUERTE",1,WHITE)
-            aviso_rotado = pygame.transform.rotate(aviso,90)
-            aviso_rect = aviso_rotado.get_rect(center = aviso_centro)
-            screen.blit(aviso_rotado,aviso_rect)
-        pygame.display.update()
+
+
         
-def dibujar_flecha(dir_cardinal,screen,lcolor,tricolor,start,end,trirad,thickness=20):
-    if(dir_cardinal == "N"):
-        arrow(screen,lcolor,tricolor,end,start,trirad,thickness=20)
-    elif(dir_cardinal == "E"):
-        inicio = [centro_flecha[0],centro_flecha[1]+int(longitud_flecha/2)]
-        fin = [centro_flecha[0],centro_flecha[1]-int(longitud_flecha/2)]
-        arrow(screen,lcolor,tricolor,inicio,fin,trirad,thickness=20)
-    elif(dir_cardinal == "S"):
-        arrow(screen,lcolor,tricolor,start,end,trirad,thickness=20)
-    
-    
+        
+def draw_velocidad(screen,velocidad):
+    screen.fill(BLACK)
+    if (velocidad!=0):
+        cx = int(screen.get_width()/2)
+        cy = int(screen.get_height()/2)
+        font_size = 480
+        circle_size = 380
+        circle_width = 30
+        circulo = circle(screen,RED,(cx-circle_offset,cy),circle_size,circle_width)
+        myfont = pygame.font.SysFont("Transport",font_size)
+        velocidad = myfont.render(str(velocidad),1,WHITE)
+        velocidad_rotada = pygame.transform.rotate(velocidad,90)
+        velocidad_rect = velocidad_rotada.get_rect(center = circulo.center)
+        screen.blit(velocidad_rotada,velocidad_rect)
+    pygame.display.update()
+
+def draw_rosa_viento(screen,direccion,velocidad = 0):
+    screen.fill(BLACK)
+    imagen = pygame.image.load(direccion)
+    imagen_escalada = pygame.transform.scale(imagen,(int(monitor_width/1.7),int(monitor_height)))
+    imagen_rotada = pygame.transform.rotate(imagen_escalada,90)
+    imagen_rect = imagen_rotada.get_rect(center=centro_flecha)
+    screen.blit(imagen_rotada,imagen_rect)
+    pygame.display.update()
+
 def arrow(screen, lcolor, tricolor, start, end, trirad, thickness=20):
     pygame.draw.line(screen, lcolor, start, end, thickness)
     rotation = (math.atan2(start[1] - end[1], end[0] - start[0])) + math.pi/2
@@ -76,9 +73,6 @@ def arrow(screen, lcolor, tricolor, start, end, trirad, thickness=20):
                                        (end[0] + trirad * math.sin(rotation + 120*rad),
                                         end[1] + trirad * math.cos(rotation + 120*rad))))
 
-    
-    
-    
 def logica_trafico(velocidad_viento,lluvia):
     if(not lluvia):
         if(velocidad_viento<=15 and velocidad_viento>10):
@@ -112,13 +106,13 @@ def wait():
                 return
                 
 v = logica_trafico(20,False)
-signal = displaySignal(v,"N")
-signal.draw(screen)
+draw_velocidad(screen,v)
+wait()
+draw_rosa_viento(screen,"N.png")
 wait()
 
 v = logica_trafico(25,False)
-newSignal = displaySignal(v,"E")
-newSignal.draw(screen)
+draw_velocidad(screen,v)
 wait()
 pygame.quit()
     
